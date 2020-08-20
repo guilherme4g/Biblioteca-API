276d490e312588cdb221a5cc50878306a4a0e6f6
jose guilherme <jgnvs6@gmail.com>
2020-08-20 00:00:21 -0300

    Book controller

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,7 +1,10 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Text.Json;
 using Biblioteca_API.Models; 
+using Biblioteca_API.Repositories;
+using System.Dynamic;
 
 namespace Biblioteca_API.Controllers
 {
@@ -9,33 +12,55 @@ namespace Biblioteca_API.Controllers
     [Route("/api/livros")]
     public class UserController : ControllerBase
     {
+
+      BookRepository bookRepository = new BookRepository();
       
         [HttpGet]
         public string get() {
-          object obj = new {  };
-          return JsonSerializer.Serialize(obj);
+          List<Book> books = bookRepository.Get();
+          return JsonSerializer.Serialize(books);
         }
 
         [HttpGet("{id}")]
-        public string get(string id) {
-          object obj = new {  };
-          return JsonSerializer.Serialize(obj);
+        public string get(int id) {
+          Book book = bookRepository.Get(id);
+          return JsonSerializer.Serialize(book);
         }
         
         [HttpPost]
         public string post([FromBody]Book book) {
-          return JsonSerializer.Serialize(book);
+          dynamic response = new ExpandoObject();
+          bool inserted = bookRepository.Create(book);
+          if (inserted) {
+            response.sucess = "Livro inserido com sucesso";
+          } else {
+            response.sucess = "Erro ao inserir editar o Livro";
+          }   
+          return JsonSerializer.Serialize(response);
         }
 
         [HttpPut("{id}")]
-        public string post(string id, [FromBody]Book book) {
-          return JsonSerializer.Serialize(book);
+        public string Update(int id, [FromBody]Book book) {
+          dynamic response = new ExpandoObject();
+          bool edited = bookRepository.Update(id, book);
+          if (edited) {
+            response.sucess = "Livro editado com sucesso";
+          } else {
+            response.sucess = "Erro ao tentar editar o Livro";
+          }   
+          return JsonSerializer.Serialize(response);
         }
 
         [HttpDelete("{id}")]
-        public string delete(string id) {
-          object obj = new {  };
-          return JsonSerializer.Serialize(obj);
+        public string delete(int id) {
+          dynamic response = new ExpandoObject();
+          bool deleted = bookRepository.Delete(id);
+          if (deleted) {
+            response.sucess = "Livro deletado com sucesso";
+          } else {
+            response.sucess = "Erro ao tentar deletar o Livro";
+          }   
+          return JsonSerializer.Serialize(response);
         }
     }
 }
