8580c802934135529b5ff19f097b9c1a06e7a944
jose guilherme <jgnvs6@gmail.com>
2020-08-19 23:26:53 -0300

    model book with validations

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,14 +1,28 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Biblioteca_API.Models{
   public class Book {
     
+    public int id {get; set;}
+
+    [Required(ErrorMessage="O título é obrigatório", AllowEmptyStrings = false)]
     public string title {get; set;}
+
+    [Required(ErrorMessage="O nome do autor é obrigatório", AllowEmptyStrings = false)]
     public string author {get; set;}
 
-    public Book(string title, string author){
+    [Required(ErrorMessage="O isbn é obrigatório", AllowEmptyStrings = false)]
+     [StringLength(13)]
+    public string isbn {get; set;}
+    public string description {get; set;}
+
+    public Book(int id, string description, string title, string author, string isbn){
+      this.id = id;
       this.title = title;
       this.author = author;
+      this.isbn = isbn;
+      this.description = description;
     }
 
   }
